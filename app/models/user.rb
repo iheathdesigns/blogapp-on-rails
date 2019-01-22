b1bfb724c71b46ef1cc112ba81f51d417c11381b
iheathdesigns <ianheath2010@gmail.com>
2019-01-22 09:31:44 -0500

    Initial Commit
    Created App

---
+++ app/models/user.rb
@@ -0,0 +1,9 @@
+class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+         
+         has_many :blog_posts
+         has_many :comments
+end
